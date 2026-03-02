f8519a7b7fc2f64e3c26216380b3c801da7bb06a
daviesjoshua268-collab <daviesjoshua268@gmail.com>
2026-03-02 11:30:55 +1100

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(bodyParser.json());
 
-/* ===== SANDBOX CONFIG (TEMP HARD-CODED) ===== */
+/* ===== SANDBOX CONFIG ===== */
 const CLIENT_ID = "3m0citah728tgv2gapuvqefs98";
 const CLIENT_SECRET = "1dbicnscqpmqpjvb5cl97j4ne1e6v7hh9achnjq3gibursfqpmi1";
 const SCOPE = "im-au-11/e74c6020-ee82-013e-a094-0a58a9feac03:c4631aa7-b3f2-4975-be6f-2d5a812e9896:3";
 const SELLER_ID = "31b75a5355e7ffa81bbfbdbccb201d2a";
-";
-
 const EMAIL_USER = "hello@jetcx.com.au";
 const EMAIL_PASS = "szdd wfjn yghv qmxj";
 
@@ -40,10 +38,13 @@ app.post("/setup-recurring", async (req, res) => {
 
     const amountInCents = Math.round(parseFloat(amount) * 100);
 
-    /* SEND DDR EMAIL */
+    /* ===== SEND DDR EMAIL ===== */
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      auth: { user: EMAIL_USER, pass: EMAIL_PASS }
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS
+      }
     });
 
     await transporter.sendMail({
@@ -52,23 +53,27 @@ app.post("/setup-recurring", async (req, res) => {
       subject: "New Recurring DDR Signed",
       html: `
         <h2>Recurring DDR</h2>
-        <p>${firstName} ${lastName}</p>
-        <p>${email}</p>
-        <p>$${amount}</p>
+        <p>Name: ${firstName} ${lastName}</p>
+        <p>Email: ${email}</p>
+        <p>Amount: $${amount}</p>
         <p>Signature: ${signature}</p>
       `
     });
 
-    /* GET ZAI TOKEN */
+    /* ===== GET ZAI TOKEN ===== */
     const tokenResponse = await axios.post(
       "https://au-0000.sandbox.auth.assemblypay.com/tokens",
       new URLSearchParams({
         grant_type: "client_credentials",
-        client_id: 3m0citah728tgv2gapuvqefs98,
-        client_secret:1dbicnscqpmqpjvb5cl97j4ne1e6v7hh9achnjq3gibursfqpmi1,
-        scope im-au-11/e74c6020-ee82-013e-a094-0a58a9feac03:c4631aa7-b3f2-4975-be6f-2d5a812e9896:3
-      }),
-      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        scope: SCOPE
+      }).toString(),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
     );
 
     const accessToken = tokenResponse.data.access_token;
